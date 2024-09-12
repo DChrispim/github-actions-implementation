f6c4aca100c64a5c6bed244b04129e3e70340208
DChrispim <brenoadsdc@gmail.com>
2024-09-12 20:17:21 -0300

    Add workflow with python file and push trigger

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # test-github-actions
 Simple GitHub repository to test how to implement automatization with GitHub Actions
+
+```bash
+├── .github
+│   └── workflows
+│       └── run-script.yml
+├── src
+│   └── scripts
+│       └── autp_anki_card_creation.py
+|   └── input
+|   └── output
+├── requirements.txt (se houver dependências)
+```
---
+++ src/scripts/python_script.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+
+input_path = os.path.join(os.getcwd(), 'src', 'input')
+output_path = os.path.join(os.getcwd(), 'src', 'output')
+
+
+def verify_number(input_file, output_file):
+    with open(input_file, 'r') as f:
+        numbers = np.array([int(linha.strip()) for linha in f.readlines()])
+
+    with open(output_file, 'w') as f_out:
+        for number in numbers:
+            if np.mod(number, 2) == 0:
+                result = f"{number} is even\n"
+            else:
+                result = f"{number} is odd\n"
+            f_out.write(result)
+
+
+# Chame a função com o caminho do arquivo de entrada e saída
+verify_number(os.path.join(input_path, 'input_numbers.txt'),
+              os.path.join(output_path, 'output_numbers.txt'))
